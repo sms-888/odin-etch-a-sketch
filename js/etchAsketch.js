d13bfbd67f7b55c499ea933835ca59cb4f293c66
sms-888 <146675392+sms-888@users.noreply.github.com>
2023-10-26 00:10:32 +0100

    Add function resetGrid()
    added function resetGrid() and added event listener to reset button

--- js/etchAsketch.js
+++ js/etchAsketch.js
@@ -3,12 +3,48 @@ function drawGrid(sideLength) {
     const flexBasis = Math.round(100 / sideLength);
     const styleText = `flex: 1 0 ${flexBasis}%; background-color:pink`;
 
+    // draw a square grid of divs
     for (i = sideLength * sideLength; i > 0; i--) {
         let gridDiv = document.createElement('div');
         gridDiv.setAttribute("class", "box")
         gridDiv.style.cssText = styleText;
+        // add event listener for mouse enter events
+        gridDiv.addEventListener('mouseenter', highlightGridBox);
         containerDiv.appendChild(gridDiv);
     }
 }
 
-drawGrid(16);
+function removeGrid() {
+    const containerDiv = document.querySelector(".container");
+    const gridDivs = containerDiv.querySelectorAll("div");
+
+    for (box of gridDivs) box.remove(); // remove each grid box
+}
+
+function resetGrid() {
+    const MAXSIDE = 100;
+    let sideLength = Infinity;
+
+    // prompt for user to enter sidelength
+    while (sideLength > MAXSIDE) {
+        sideLength = prompt("Enter number of squares per side (Max 100): ", "");
+    }
+    // remove old grid
+    removeGrid();
+    // draw new grid
+    drawGrid(sideLength);
+}
+
+// highlight grid box in blue
+function highlightGridBox(e) {
+    e.target.style.background = 'blue';
+}
+
+function initialise() {
+    const btn = document.querySelector('button');
+    btn.addEventListener('click', resetGrid);
+    drawGrid(16);
+}
+
+initialise();
+
